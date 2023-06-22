2357c855480030ef8b02582eed4c75122a15e1a6
ghopunk <108220530+ghopunk@users.noreply.github.com>
2023-06-23 03:29:15 +0700

    Update Headers.php

--- src/Headers.php
+++ src/Headers.php
@@ -328,18 +328,21 @@ class Headers {
 	
 	public static function parseHeaders( $headers ){
 		$head = array();
-		if( !empty($headers) && is_array($headers) ) {
+		if( !empty($headers) ) {
+			if( !is_array($headers) ) {
+				$headers = explode( "\n", $headers );
+			}
 			foreach( $headers as $k=>$v ){
-				$t	= explode( ':', $v, 2 );
-				if( isset( $t[1] ) ) {
-					$head[ trim($t[0]) ] = trim( $t[1] );
-				} else {
-					$head[] = $v;
-					if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) ) {
-						$head['response_code'] = intval($out[1]);
-					} elseif( preg_match( "#Content-Type#i",$v, $out ) ) {
-						$head['content_type'] = intval($out[1]);
-					}
+				$t = explode( ':', $v, 2 );
+				if( preg_match( "#Content-Type#i", $v, $out ) ) {
+					$c_type	= explode( ';', $t[1] );
+					$head['mime_type']	= trim($c_type[0]);
+				} elseif( preg_match( '#filename="([^"]+)"#i', $v, $out ) ) {
+					$head['filename']	= trim($out[1]);
+				} elseif( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) ) {
+					$head['response_code'] = intval($out[1]);
+				} elseif( isset( $t[1] ) ) {
+					$head[ trim($t[0]) ]	= trim( $t[1] );
 				}
 			}
 		}
